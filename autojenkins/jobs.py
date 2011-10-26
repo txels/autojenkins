6bdc932b15b1f53636ef0cb070d88d7c1168a4c6
Carles Barrobés <carles.barrobes@glassesdirect.com>
2011-10-26 09:14:37 +0100

    Packaged into Jenkins class

--- autojenkins/jobs.py
+++ autojenkins/jobs.py
@@ -1,42 +1,52 @@
 import requests
 
-ROOT = 'http://jenkins.pe.local/'
 
 API = '/api/python'
-DELETE = ROOT + 'job/{0}/doDelete'
-BUILD = ROOT + 'job/{0}/build'
-CONFIG = ROOT + 'job/{0}/config.xml'
-NEWJOB = ROOT + 'createItem'
-JOBINFO = ROOT + 'job/{0}' + API
-LAST_SUCCESS = ROOT + 'job/{0}/lastSuccessfulBuild' + API
-TEST_REPORT = ROOT + 'job/{0}/lastSuccessfulBuild/testReport' + API
+DELETE = '{0}/job/{1}/doDelete'
+BUILD = '{0}/job/{1}/build'
+CONFIG = '{0}/job/{1}/config.xml'
+NEWJOB = '{0}/createItem'
+JOBINFO = '{0}/job/{1}' + API
+LAST_SUCCESS = '{0}/job/{1}/lastSuccessfulBuild' + API
+TEST_REPORT = '{0}/job/{1}/lastSuccessfulBuild/testReport' + API
 
 
-def build(jobname):
-    """Trigger Jenkins to build a job."""
-    return requests.post(BUILD.format(jobname))
+class Jenkins(object):
+
+    def __init__(self, base_url='http://jenkins.pe.local'):
+        self.ROOT = base_url
 
 
-def job_info(jobname):
-    """Get all information for a job as a Python object (dicts & lists)."""
-    response = requests.get(JOBINFO.format(jobname))
-    return eval(response.content)
+    def _build_url(self, command, *args):
+        """Build the proper Jenkins URL for the command."""
+        return command.format(self.ROOT, *args)
 
 
-def copy(jobname, copy_from='template'):
-    """
-    Copy a job from another one (by default from one called ``template``).
-    """
-    params = {'name': jobname, 'mode': 'copy', 'from': copy_from}
-    return requests.post(NEWJOB, params=params)
+    def build(self, jobname):
+        """Trigger Jenkins to build a job."""
+        return requests.post(self._build_url(BUILD, jobname))
 
 
-def get_config_xml(jobname):
-    """Get the ``config.xml`` file that contains the full job definition."""
-    response = requests.get(CONFIG.format(jobname))
-    return response.content
+    def job_info(self, jobname):
+        """Get all information for a job as a Python object (dicts & lists)."""
+        response = requests.get(self._build_url(JOBINFO, jobname))
+        return eval(response.content)
 
 
-def delete(jobname):
-    """Delete a job."""
-    return requests.post(DELETE.format(jobname))
+    def copy(self, jobname, copy_from='template'):
+        """
+        Copy a job from another one (by default from one called ``template``).
+        """
+        params = {'name': jobname, 'mode': 'copy', 'from': copy_from}
+        return requests.post(self._build_url(NEWJOB), params=params)
+
+
+    def get_config_xml(self, jobname):
+        """Get the ``config.xml`` file that contains the full job definition."""
+        response = requests.get(self._build_url(CONFIG, jobname))
+        return response.content
+
+
+    def delete(self, jobname):
+        """Delete a job."""
+        return requests.post(self._build_url(DELETE, jobname))
