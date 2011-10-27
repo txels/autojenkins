f326a2896029951f8356d9551e7661ff135215aa
Carles Barrobés <carles.barrobes@glassesdirect.com>
2011-10-27 23:01:25 +0100

    Added creation of fully configured jobs using Jinja2 template variables

--- autojenkins/jobs.py
+++ autojenkins/jobs.py
@@ -1,4 +1,5 @@
 import requests
+from jinja2 import Template
 
 
 API = '/api/python'
@@ -16,28 +17,69 @@ class Jenkins(object):
     def __init__(self, base_url='http://jenkins.pe.local'):
         self.ROOT = base_url
 
-
     def _build_url(self, command, *args):
-        """Build the proper Jenkins URL for the command."""
+        """
+        Build the proper Jenkins URL for the command.
+        """
         return command.format(self.ROOT, *args)
 
-
     def job_info(self, jobname):
-        """Get all information for a job as a Python object (dicts & lists)."""
+        """
+        Get all information for a job as a Python object (dicts & lists).
+        """
         response = requests.get(self._build_url(JOBINFO, jobname))
         return eval(response.content)
 
-
     def get_config_xml(self, jobname):
-        """Get the ``config.xml`` file that contains the full job definition."""
+        """
+        Get the ``config.xml`` file that contains the job definition.
+        """
         response = requests.get(self._build_url(CONFIG, jobname))
         return response.content
 
-
     def build(self, jobname):
-        """Trigger Jenkins to build a job."""
+        """
+        Trigger Jenkins to build a job.
+        """
         return requests.post(self._build_url(BUILD, jobname))
 
+    def create(self, jobname, config_file, **context):
+        """
+        Create a job from a configuration file.
+        """
+        params = {'name': jobname}
+        with open(config_file) as file:
+            content = file.read()
+
+        template = Template(content)
+        content = template.render(**context)
+
+        return requests.post(self._build_url(NEWJOB),
+                             data=content,
+                             params=params,
+                             headers={'Content-Type': 'application/xml'})
+
+    def create_copy(self, jobname, template_job, enable=True, **context):
+        """
+        Create a job from a template job.
+        """
+        config = self.get_config_xml(template_job)
+        with open('config.xml', 'w') as file:
+            file.write(config)
+        # remove stupid quotes added by Jenkins
+        config = config.replace('&quot;{{branch}}&quot;',
+                                '{{branch}}')
+
+        template_config = Template(config)
+        config = template_config.render(**context)
+        if enable:
+            config = config.replace('<disabled>true</disabled>',
+                                    '<disabled>false</disabled>')
+
+        return requests.post(self._build_url(NEWJOB),
+                             data=config,
+                             params={'name': jobname},
+                             headers={'Content-Type': 'application/xml'})
 
     def copy(self, jobname, copy_from='template'):
         """
@@ -46,19 +88,22 @@ class Jenkins(object):
         params = {'name': jobname, 'mode': 'copy', 'from': copy_from}
         return requests.post(self._build_url(NEWJOB), params=params)
 
-
     def delete(self, jobname):
-        """Delete a job."""
+        """
+        Delete a job.
+        """
         return requests.post(self._build_url(DELETE, jobname))
 
-
     def last_success(self, jobname):
-        """Return information about the last successful build."""
+        """
+        Return information about the last successful build.
+        """
         return requests.post(self._build_url(LAST_SUCCESS, jobname))
 
-
     def last_result(self, jobname):
-        """Obtain results from last execution."""
+        """
+        Obtain results from last execution.
+        """
         last_result_url = self.job_info(jobname)['lastBuild']['url']
         response = requests.get(last_result_url + API)
         return eval(response.content)
---
+++ autojenkins/test_run.py
@@ -0,0 +1,16 @@
+from os import path
+from autojenkins import Jenkins
+
+
+if __name__ == '__main__':
+    j = Jenkins()
+    # dir = path.dirname(path.dirname(__file__))
+    # config_file = path.join(dir, 'templates/story-dev-job.xml')
+    response = j.delete('aaa')
+    response = j.create_copy('aaa', 'template',
+                        repo='mbf-warehouse-screens',
+                        branch='us544_login',
+                        package='warehouse_screens')
+    print(response.status_code)
+    if response.status_code == 200:
+        j.build('aaa')
