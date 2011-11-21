250701318dfdc3fd22d90915092c3e451b245cd8
Carles Barrobés <carles.barrobes@glassesdirect.com>
2011-11-21 15:25:03 +0000

    Bumped version to 0.4.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='autojenkins',
     description='Jenkins Remote Control Library',
     long_description=long_description,
-    version='0.4.2',
+    version='0.4.3',
     author='Carles Barrobés',
     author_email='carles@barrobes.com',
     url='https://github.com/txels/autojenkins',
