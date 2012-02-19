122669f8cf880c633851d6a34762fdf8955aba15
Carles Barrobés <carles.barrobes@glassesdirect.com>
2012-02-19 22:12:44 +0000

    Fix documents build path

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,8 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+docs_root = os.path.dirname(__file__)
+sys.path.insert(0, os.path.split(docs_root)[0])
 
 # -- General configuration -----------------------------------------------------
 
